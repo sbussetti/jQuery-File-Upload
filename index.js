7c4b8b5cd7bdcbe3f17f0a638d27f674089b50f4
sbussetti <steve.bussetti@gmail.com>
2015-01-28 15:11:25 -0500

    export options

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./js/jquery.fileupload.js');
+/* jshint node: true */
+module.exports = {
+  audio: require('./js/jquery.fileupload-audio.js'),
+  image: require('./js/jquery.fileupload-image.js'),
+  jquery_ui: require('./js/jquery.fileupload-jquery-ui.js'),
+  process: require('./js/jquery.fileupload-process.js'),
+  ui: require('./js/jquery.fileupload-ui.js'),
+  validate: require('./js/jquery.fileupload-validate.js'),
+  video: require('./js/jquery.fileupload-video.js'),
+  fileupload: require('./js/jquery.fileupload.js'),
+  iframe_transport: require('./js/iframe-transport.js')
+};
